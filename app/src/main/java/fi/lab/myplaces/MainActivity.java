92a6bd0c36d6b581beaf4a2b11a9e12c779d6168
Paavo Virtanen <hellothere@paavovirtanen.fi>
2023-05-08 10:25:31 +0300

    Init
    Added two framelayouts as fragments and updated libraries

--- app/src/main/java/fi/lab/myplaces/MainActivity.java
+++ app/src/main/java/fi/lab/myplaces/MainActivity.java
@@ -3,6 +3,11 @@ package fi.lab.myplaces;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
